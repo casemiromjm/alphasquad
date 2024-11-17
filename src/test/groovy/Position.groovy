a42158d3a0104cc8e1ecdeac01673e3e77dc52cb
Hugo Alves <hribalves@gmail.com>
2024-11-17 15:32:38 +0000

    Small update on build.gradle and added tests for the Position class

---
+++ src/test/groovy/Position.groovy
@@ -0,0 +1,51 @@
+import spock.lang.Specification
+
+class TestPosition extends Specification{
+
+    def "test getX"(){
+        given:
+        Position position = new Position(1,2)
+
+        when:
+        int x = position.getX();
+
+        then:
+        x == 1;
+    }
+
+    def "test getY"(){
+        given:
+        Position position = new Position(1,2)
+
+        when:
+        int y = position.getY();
+
+        then:
+        y == 2;
+    }
+
+    def "test setX"(){
+        given:
+        Position position = new Position(1,2)
+
+        when:
+        position.setX(3)
+        int x = position.getX();
+
+        then:
+        x == 3;
+    }
+
+    def "test setY"(){
+        given:
+        Position position = new Position(1,2)
+
+        when:
+        position.setY(30)
+        int y = position.getY();
+
+        then:
+        y == 30;
+    }
+
+}
